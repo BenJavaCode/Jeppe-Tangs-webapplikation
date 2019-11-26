c776204ddff107eb8a62b636e00ffa0f191aad53
lokstorm <55553446+lokstorm@users.noreply.github.com>
2019-11-26 11:22:02 +0100

    First commit

---
+++ src/test/java/com/jeppetang/webphotoapp/PhotoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeppetang.webphotoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
