0e7b3fe4b7c9a66a29d789e42c66bea2b979fcc6
adilhussain <adil0276@stud.kea.dk>
2019-11-28 09:20:37 +0100

    cv

---
+++ src/main/java/com/jeppetang/webphotoapp/Repositories/CvRepository.java
@@ -0,0 +1,16 @@
+package com.jeppetang.webphotoapp.Repositories;
+
+import com.jeppetang.webphotoapp.Models.Cv;
+
+import java.util.List;
+
+public interface CvRepository {
+
+    public int saveCv(Cv cv);
+    public int updateCv(Cv cv);
+
+
+
+
+
+}
---
+++ src/main/java/com/jeppetang/webphotoapp/Repositories/CvRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.jeppetang.webphotoapp.Repositories;
+
+import com.jeppetang.webphotoapp.Models.Cv;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CvRepositoryImpl implements CvRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public int saveCv(Cv cv) {
+
+        return jdbcTemplate.update(
+                "insert into cv(title, mainText, mailAddress, phoneNumber, previousWork, cvPicture) values (?,?,?,?,?,?)"
+        );
+
+
+    }
+
+    @Override
+    public int updateCv(Cv cv){
+
+        return jdbcTemplate.update(
+                "update cv set title = ?, mainText = ?, mailAdress = ?, phoneNumber = ?, previousWork = ?, cvPicture = ? "
+        );
+    }
+
+
+}
