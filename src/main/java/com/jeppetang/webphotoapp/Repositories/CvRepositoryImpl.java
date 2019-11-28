63bc65ae964984f4c134c11fbc4c6ec433f458a6
adilhussain <adil0276@stud.kea.dk>
2019-11-28 11:31:45 +0100

    cv

--- src/main/java/com/jeppetang/webphotoapp/Repositories/CvRepositoryImpl.java
+++ src/main/java/com/jeppetang/webphotoapp/Repositories/CvRepositoryImpl.java
@@ -1,9 +1,13 @@
 package com.jeppetang.webphotoapp.Repositories;
 
 import com.jeppetang.webphotoapp.Models.Cv;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.logging.Logger;
+
 
 @Repository
 public class CvRepositoryImpl implements CvRepository {
@@ -11,8 +15,14 @@ public class CvRepositoryImpl implements CvRepository {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    private final Logger log = (Logger) LoggerFactory.getLogger(this.getClass());
+
+
+
     @Override
     public int saveCv(Cv cv) {
+
+        log.info("Saving Cv..");
 
         return jdbcTemplate.update(
                 "insert into cv(title, mainText, mailAddress, phoneNumber, previousWork, cvPicture) values (?,?,?,?,?,?)"
@@ -23,6 +33,8 @@ public class CvRepositoryImpl implements CvRepository {
 
     @Override
     public int updateCv(Cv cv){
+
+        log.info("Updating Cv..");
 
         return jdbcTemplate.update(
                 "update cv set  mainText = ?, mailAdress = ?, phoneNumber = ?, previousWork = ?, cvPicture = ? where title=?"
